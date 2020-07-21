dfc97a584a39eb8a3bbd7586575f03ce1261aaec
Adam Mikacich <adammikacich@gmail.com>
2020-07-21 01:39:24 -0700

    Create buckets on start up

--- backend/modules/storage/index.js
+++ backend/modules/storage/index.js
@@ -9,8 +9,32 @@ class Storage {
   async init() {
     const minioClient = new Minio.Client(config.data.storage);
 
+    await updateBuckets();
+
     debug('init');
     return;
+  }
+
+  async updateBucket(name) {
+    return new Promise((resolve, reject) => {
+      minioClient.bucketExists(name, function(err, exists) {
+        if (err) return reject(err);
+        if (exists) {
+          debug('found bucket', name);
+          return resolve();
+        } else {
+          minioClient.makeBucket(name, 'us-west-1', function(err2) {
+            if (err2) return reject(err2);
+            debug('made bucket', name);
+            return resolve();
+          })
+        }
+      })
+    });
+  }
+
+  async updateBuckets() {
+    return updateBucket('assets');
   }
 
   async saveFile(file) {
