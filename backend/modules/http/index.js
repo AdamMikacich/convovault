cd7cb8a554b87311149ec3dcdba259cad1f20672
Adam Mikacich <adammikacich@gmail.com>
2020-07-21 16:25:44 -0700

    Reference correct directory

--- backend/modules/http/index.js
+++ backend/modules/http/index.js
@@ -15,10 +15,10 @@ class HTTP {
     app.use(bodyParser.text());
     app.use(bodyParser.urlencoded({ extended: true }));
 
-    app.use('/', express.static(path.join(__dirname, './../../frontend/dist')));
+    app.use('/', express.static(path.join(__dirname, './../../../frontend/dist')));
     
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, './../../frontend/dist/index.html'));
+      res.sendFile(path.join(__dirname, './../../../frontend/dist/index.html'));
     });
 
     app.post('/slack', async (req, res) => {
