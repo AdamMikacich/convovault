ee84796456caa654abae1586a98ccb7fa9be5bc2
Adam Mikacich <adammikacich@gmail.com>
2020-07-21 01:40:23 -0700

    Fix this references

--- backend/modules/storage/index.js
+++ backend/modules/storage/index.js
@@ -7,9 +7,9 @@ const request = require('request');
 
 class Storage {
   async init() {
-    const minioClient = new Minio.Client(config.data.storage);
+    this.minioClient = new Minio.Client(config.data.storage);
 
-    await updateBuckets();
+    await this.updateBuckets();
 
     debug('init');
     return;
@@ -17,13 +17,13 @@ class Storage {
 
   async updateBucket(name) {
     return new Promise((resolve, reject) => {
-      minioClient.bucketExists(name, function(err, exists) {
+      this.minioClient.bucketExists(name, function(err, exists) {
         if (err) return reject(err);
         if (exists) {
           debug('found bucket', name);
           return resolve();
         } else {
-          minioClient.makeBucket(name, 'us-west-1', function(err2) {
+          this.minioClient.makeBucket(name, 'us-west-1', function(err2) {
             if (err2) return reject(err2);
             debug('made bucket', name);
             return resolve();
@@ -34,7 +34,7 @@ class Storage {
   }
 
   async updateBuckets() {
-    return updateBucket('assets');
+    return this.updateBucket('assets');
   }
 
   async saveFile(file) {
